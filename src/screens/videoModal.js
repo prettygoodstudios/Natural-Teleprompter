5f921c73c9528118a45567303638983569813704
Miguel Rust <hava12soft@gmail.com>
2019-04-18 21:02:19 -0600

    Built out the ability to save video to camera roll.

--- src/screens/videoModal.js
+++ src/screens/videoModal.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, Text, ToucableOpacity} from "react-native";
+import {View, Text, TouchableOpacity, CameraRoll} from "react-native";
 import {connect} from "react-redux";
 import {Video} from "expo";
 
@@ -7,6 +7,20 @@ import * as actions from "../actions";
 
 
 import Modal from "../components/modal";
+import styles from "../styles";
+
+const VideoModalButton = (props) => {
+    const {content, onPress} = props;
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>
+                    {content}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
 
 
 class VideoModal extends Component {
@@ -17,6 +31,12 @@ class VideoModal extends Component {
 
         }
 
+    }
+
+    saveVideo = async () => {
+        const video = await CameraRoll.saveToCameraRoll(this.props.uri, "video");
+        alert("Successfully Saved Video To Photos!");
+        this.props.closeVideo();
     }
 
     render(){
@@ -37,18 +57,19 @@ class VideoModal extends Component {
                         volume={1.0}
                         isMuted={false}
                         resizeMode="cover"
-                        shouldPlay
                         useNativeControls
                         style={{ width: "100%", height: 500 }}
                         />
                 </View>
+                <VideoModalButton onPress={this.saveVideo} content="Save To Photos"/>
+                <VideoModalButton onPress={this.props.closeVideo} content="Discard" />
+
             </Modal>
         )
     }
 }
 
 function mapStateToProps(state){
-    console.log(state);
     const {video} = state;
     return{
         ...video
